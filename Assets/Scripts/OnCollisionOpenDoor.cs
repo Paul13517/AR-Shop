3d927f742914f68f20d55af8ce305b87376110c6
Paul17135 <pavelfsn@yandex.ru>
2020-06-27 01:27:49 +0300

    Initial commit

---
+++ Assets/Scripts/OnCollisionOpenDoor.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class OnCollisionOpenDoor : MonoBehaviour {
+
+    public GameObject door;
+    public float moveSpeed = 2f;
+    private float closeDoor, openDoor = 90f;
+    private Coroutine coroutine;
+
+    private void OnTriggerEnter(Collider other) {
+        stopRotate(other);
+
+        if(coroutine == null && other.CompareTag("Door"))
+           coroutine = StartCoroutine(RotateDoor(openDoor, true));
+    }
+
+    private void OnTriggerExit(Collider other) {
+        stopRotate(other);
+
+        if (coroutine == null && other.CompareTag("Door"))
+            coroutine = StartCoroutine(RotateDoor(closeDoor, false));
+    }
+
+    IEnumerator RotateDoor(float yPos, bool needOpen) {
+        if(needOpen) {
+            while(door.transform.localRotation.y < yPos) {
+                RotateObject(yPos);
+                yield return null;
+            }
+        } else {
+            while (door.transform.localRotation.y > yPos) {
+                 RotateObject(yPos);
+                 yield return null;
+             }
+        }
+     }
+     void RotateObject(float yPos) {
+    door.transform.localRotation = Quaternion.Slerp(
+        door.transform.localRotation,
+        Quaternion.Euler(0, yPos, 0),
+        moveSpeed * Time.deltaTime
+        );
+     } 
+
+     void stopRotate(Collider other) {
+          if (coroutine != null && other.CompareTag("Door")) {
+          StopCoroutine(coroutine);
+          coroutine = null;
+       }
+
+
+     }
+}
---
+++ Assets/Scripts/PlaceObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PlaceObject : MonoBehaviour {
+
+    public GameObject midAir;
+
+    private void OnMouseUpAsButton() {
+        midAir.SetActive(true);
+
+    }
+}
